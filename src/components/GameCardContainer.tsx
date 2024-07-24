16b08f2cc477969026f585acb4221b0836582342
Sambhav Rana <sambhavrana27@gmail.com>
2024-07-25 05:14:46 +0530

    refactor: Removed duplicate styles

---
+++ src/components/GameCardContainer.tsx
@@ -0,0 +1,17 @@
+import { Box } from '@chakra-ui/react'
+import { ReactNode } from 'react'
+
+interface Props {
+    children: ReactNode;  // ReactNode is a TypeScript type that represents a component's children in React
+}
+
+
+const GameCardContainer = ({ children }: Props) => {
+    return (
+        <Box width='300px' borderRadius={10} overflow="hidden">
+            {children}
+        </Box>
+    )
+}
+
+export default GameCardContainer
