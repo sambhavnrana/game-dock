75b8e5d3e9914f45b5dcb3e83f46b66c40914d55
Sambhav Rana <sambhavrana27@gmail.com>
2024-07-30 19:11:23 +0530

    feat: Highlight selected Genre

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,12 @@ function App() {
     }} templateColumns={{
       base: '1fr',
       lg: '200px 1fr',
-
     }}
     >
       <GridItem gridArea="nav" ><Navbar /></GridItem>
       <Show above="lg">
         <GridItem area="aside" paddingX={5} >
-          <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)} />
-
+          <GenreList selectedGenre={selectedGenre} onSelectGenre={(genre) => setSelectedGenre(genre)} />
         </GridItem>
       </Show>
       <GridItem area="main" >
